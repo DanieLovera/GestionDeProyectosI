c4fcb007eec0a069fe106f9286d74c11b3538120
Martin Sotelo <lmsotelo@fi.uba.ar>
2025-10-18 19:07:14 -0300

    individualExpenses.js updated to endpoint

--- frontend/src/apis/individualExpenses.js
+++ frontend/src/apis/individualExpenses.js
@@ -1,9 +1,14 @@
-import { apiGet, apiPost } from "./client.js";
+import { apiGet, apiPost, apiDelete } from "./client.js";
 
-export const getIndividualExpenses = async () => {
-  return await apiGet("/individual-expenses");
+export const getIndividualExpenses = async (params = "") => {
+  const q = params ? `?${params}` : "";
+  return await apiGet(`/individual-expenses${q}`);
 };
 
 export const addIndividualExpense = async (expense) => {
   return await apiPost("/individual-expenses", expense);
+};
+
+export const deleteIndividualExpense = async (id) => {
+  return await apiDelete(`/individual-expenses/${id}`);
 };
