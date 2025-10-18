30ecd1657a634f20c2bdd20d9ae51c424b96acb6
Martin Sotelo <lmsotelo@fi.uba.ar>
2025-10-18 19:07:41 -0300

    payments use data from backend

--- frontend/src/apis/payments.js
+++ frontend/src/apis/payments.js
@@ -1,16 +1,10 @@
-import payments from "../mocks/payments";
+import { apiGet, apiPost } from "./client.js";
 
-const mockDataFetch = (data, delay = 600) => new Promise((resolve) => setTimeout(() => resolve(data), delay));
-
-export const getPayments = async () => {
-  const response = await mockDataFetch(payments, 500);
-  return response;
+export const getPayments = async (query = "") => {
+  const q = query ? `?${query}` : "";
+  return apiGet(`/payments${q}`);
 };
 
 export const addPayment = async (payment) => {
-  const newPayment = { id: Date.now(), ...payment };
-  // Simular latencia y persistencia en memoria
-  await mockDataFetch(null, 400);
-  payments.push(newPayment);
-  return newPayment;
+  return apiPost("/payments", payment);
 };
